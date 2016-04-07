3d9c10a72f85ffc7929b280fbb0df8f26ca81fb9
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-04-07 11:39:42 +0100

    extract entry schema constant

--- tests/test_entry_resource.py
+++ tests/test_entry_resource.py
@@ -4,6 +4,26 @@ import requests
 from urllib.parse import urljoin
 from jsonschema import validate
 
+# This schema should always represent the response json specified at <http://openregister.github.io/specification/#entry-resource>
+ENTRY_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "entry-number": {
+            "type": "string",
+            "pattern": "^\d+$"
+        },
+        "item-hash": {
+            "type": "string",
+            "pattern": "^sha-256:[a-f\d]{64}$"
+        },
+        "entry-timestamp": {
+            "type": "string",
+            "pattern": "^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$"
+        }
+    },
+    "required": ["entry-number", "item-hash", "entry-timestamp"],
+    "additionalProperties": False
+}
 
 class TestEntryResourceJson(object):
     @pytest.fixture
@@ -14,25 +34,4 @@ class TestEntryResourceJson(object):
         assert response.headers['content-type'] == 'application/json'
 
     def test_response_contents(self, response):
-        #This schema should always represent the response json specified at <http://openregister.github.io/specification/#entry-resource>
-        entry_schema = {
-            "type": "object",
-            "properties": {
-                "entry-number": {
-                    "type": "string",
-                    "pattern": "^\d+$"
-                },
-                "item-hash": {
-                    "type": "string",
-                    "pattern": "^sha-256:[a-f\d]{64}$"
-                },
-                "entry-timestamp": {
-                    "type": "string",
-                    "pattern": "^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$"
-                }
-            },
-            "required": ["entry-number", "item-hash", "entry-timestamp"],
-            "additionalProperties": False
-        }
-
-        validate(response.json(), entry_schema)
+        validate(response.json(), ENTRY_SCHEMA)
