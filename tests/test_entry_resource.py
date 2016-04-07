704a4f9fcb16304ee42e0385a3753bcf17a1d782
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-04-07 12:17:01 +0100

    test for yaml entry representation
    Not yet implemented; expected to fail

--- tests/test_entry_resource.py
+++ tests/test_entry_resource.py
@@ -1,8 +1,10 @@
 import pytest
 import requests
+import yaml
 
-from urllib.parse import urljoin
 from jsonschema import validate
+from urllib.parse import urljoin
+from werkzeug.http import parse_options_header
 
 # This schema should always represent the response json specified at <http://openregister.github.io/specification/#entry-resource>
 ENTRY_SCHEMA = {
@@ -35,3 +37,17 @@ class TestEntryResourceJson(object):
 
     def test_response_contents(self, response):
         validate(response.json(), ENTRY_SCHEMA)
+
+
+@pytest.mark.xfail
+class TestEntryResourceYaml(object):
+    @pytest.fixture
+    def response(self, endpoint):
+        return requests.get(urljoin(endpoint, 'entry/1.yaml'))
+
+    def test_content_type(self, response):
+        assert parse_options_header(response.headers['content-type']) \
+            == ('text/yaml', {'charset':'UTF-8'})
+
+    def test_response_contents(self, response):
+        validate(yaml.load(response.text), ENTRY_SCHEMA)
