898b0d35d041ba36f01e1f277ac1d0afca09a634
Om Sharma <foromsharma@gmail.com>
2016-04-11 17:02:01 +0100

    Update from suggestion in PR review

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,9 @@ import pytest
 def pytest_addoption(parser):
     parser.addoption("--endpoint", action="append",
         help="register endpoints to test")
-    parser.addoption("--register_register_endpoint", help="register endpoints to test")
+
 
 def pytest_generate_tests(metafunc):
     if 'endpoint' in metafunc.fixturenames:
         metafunc.parametrize("endpoint",
             metafunc.config.option.endpoint)
-
-@pytest.fixture(scope="module")
-def register_register_endpoint(request):
-    return request.config.getoption("--register_register_endpoint")
--- tests/test_item_resource.py
+++ tests/test_item_resource.py
@@ -8,23 +8,18 @@ from urllib.parse import urljoin
 class TestItemResourceJson(object):
     @pytest.fixture
     def response(self, endpoint):
-        self.register_name = re.sub(r'http[s]?://([^\.]+)(.*)', r'\1', endpoint)
-
         entry = requests.get(urljoin(endpoint, 'entry/1.json'))
 
         item_hash = entry.json()['item-hash']
 
         return requests.get(urljoin(endpoint, 'item/' + item_hash + '.json'))
 
-    def test_response_contents(self, response, register_register_endpoint ):
-        register_data = requests.get(
-            urljoin(register_register_endpoint, '/record/' + self.register_name + '.json'))
-
-        register_fields = register_data.json()['fields']
+    def test_response_contents(self, response, endpoint):
+        register_data = requests.get(urljoin(endpoint, '/register.json'))
+        register_fields = register_data.json()['record']['entry']['fields']
 
         assert set(response.json().keys()).issubset(register_fields), \
             'Item json does not match fields specified in regsiter register'
-
 
     def test_content_type(self, response):
         assert response.headers['content-type'] == 'application/json'
