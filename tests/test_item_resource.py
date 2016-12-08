1e8398c4a9c2c2468e241c0b86a14824e9319751
Chris Duraj <christopherduraj@gmail.com>
2016-12-08 11:01:52 +0000

    Create test base class for item_resource

--- tests/test_item_resource.py
+++ tests/test_item_resource.py
@@ -8,13 +8,28 @@ from urllib.parse import urljoin
 from werkzeug.http import parse_options_header
 
 
-class TestItemResourceJson(object):
+class TestResourceBase(object):
+    def __init__(self, resource_type):
+        self.resource_type = resource_type
+
     @pytest.fixture
     def response(self, endpoint):
         entry = requests.get(urljoin(endpoint, 'entry/1.json'))
         item_hash = entry.json()['item-hash']
+        return requests.get(urljoin(endpoint, 'item/%s.%s' % (item_hash, self.resource_type)))
 
-        return requests.get(urljoin(endpoint, 'item/' + item_hash + '.json'))
+    def get_schema(endpoint):
+        register_data = requests.get(urljoin(endpoint, '/register.json'))
+        register_fields = register_data.json()['register-record']['fields']
+
+        validator = CSVValidator(register_fields)
+        validator.add_header_check()
+        return validator
+
+
+class TestItemResourceJson(TestResourceBase):
+    def __init__(self):
+        super().__init__('json')
 
     def test_response_contents(self, response, endpoint):
         register_data = requests.get(urljoin(endpoint, '/register.json'))
@@ -27,13 +42,9 @@ class TestItemResourceJson(object):
         assert response.headers['content-type'] == 'application/json'
 
 
-class TestItemResourceYaml(object):
-    @pytest.fixture
-    def response(self, endpoint):
-        entry = requests.get(urljoin(endpoint, 'entry/1.json'))
-        item_hash = entry.json()['item-hash']
-
-        return requests.get(urljoin(endpoint, 'item/' + item_hash + '.yaml'))
+class TestItemResourceYaml(TestResourceBase):
+    def __init__(self):
+        super().__init__('yaml')
 
     def test_response_contents(self, response, endpoint):
         register_data = requests.get(urljoin(endpoint, '/register.json'))
@@ -47,16 +58,12 @@ class TestItemResourceYaml(object):
                == ('text/yaml', {'charset': 'UTF-8'})
 
 
-class TestItemResourceCsv(object):
-    @pytest.fixture
-    def response(self, endpoint):
-        entry = requests.get(urljoin(endpoint, 'entry/1.json'))
-        item_hash = entry.json()['item-hash']
-
-        return requests.get(urljoin(endpoint, 'item/' + item_hash + '.csv'))
+class TestItemResourceCsv(TestResourceBase):
+    def __init__(self):
+        super().__init__('csv')
 
     def test_response_contents(self, response, endpoint):
-        csv_schema = get_schema(endpoint)
+        csv_schema = self.get_schema(endpoint)
         problems = csv_schema.validate(csv.reader(response.text.split('\r\n')))
 
         assert problems == [], \
@@ -67,16 +74,12 @@ class TestItemResourceCsv(object):
                == ('text/csv', {'charset': 'UTF-8'})
 
 
-class TestItemResourceTsv(object):
-    @pytest.fixture
-    def response(self, endpoint):
-        entry = requests.get(urljoin(endpoint, 'entry/1.json'))
-        item_hash = entry.json()['item-hash']
-
-        return requests.get(urljoin(endpoint, 'item/' + item_hash + '.tsv'))
+class TestItemResourceTsv(TestResourceBase):
+    def __init__(self):
+        super().__init__('tsv')
 
     def test_response_contents(self, response, endpoint):
-        tsv_schema = get_schema(endpoint)
+        tsv_schema = self.get_schema(endpoint)
         problems = tsv_schema.validate(csv.reader(response.text.split('\n'), delimiter='\t'))
 
         assert problems == [], \
@@ -87,13 +90,9 @@ class TestItemResourceTsv(object):
                == ('text/tab-separated-values', {'charset': 'UTF-8'})
 
 
-class TestItemResourceTtl(object):
-    @pytest.fixture
-    def response(self, endpoint):
-        entry = requests.get(urljoin(endpoint, 'entry/1.json'))
-        item_hash = entry.json()['item-hash']
-
-        return requests.get(urljoin(endpoint, 'item/' + item_hash + '.ttl'))
+class TestItemResourceTtl(TestResourceBase):
+    def __init__(self):
+        super().__init__('ttl')
 
     def test_response_contents(self, response, endpoint, entry_ttl_schema, register_domain):
         register_data = requests.get(urljoin(endpoint, '/register.json'))
@@ -113,12 +112,3 @@ class TestItemResourceTtl(object):
     def test_content_type(self, response):
         assert parse_options_header(response.headers['content-type']) \
                == ('text/turtle', {'charset': 'UTF-8'})
-
-
-def get_schema(endpoint):
-    register_data = requests.get(urljoin(endpoint, '/register.json'))
-    register_fields = register_data.json()['register-record']['fields']
-
-    validator = CSVValidator(register_fields)
-    validator.add_header_check()
-    return validator
